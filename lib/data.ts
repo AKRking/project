321c04032e08687a7dde8e25b2d9c6ac0f7354ee
AKRking <engineerkabircuet@gmail.com>
2024-12-01 13:04:03 +0600

    new

--- lib/data.ts
+++ lib/data.ts
@@ -1,14 +1,1018 @@
-export const searchData = `ACCOUNTING 9706
-"AS (Paper 1 and Paper 2) || A2 (Paper 3 and paper 4)
+export const searchData = `
+Accounting 0452
+ IG-ACC-qp1-2019 to 2024 (+৳ 490)
+
+ IG-ACC-qp1-2016 to 2018 (+৳ 390)
+
+ IG-ACC-qp1-2012 to 2015 (+৳ 480)
+
+ IG-ACC-qp1-2002 to 2011 (+৳ 380)
+
+ IG-ACC-qp2-2021 to 2024 (+৳ 440)
+
+ IG-ACC-qp2-2018 to 2020 (+৳ 390)
+
+ IG-ACC-qp2-2014 to 2017 (+৳ 490)
+
+ IG-ACC-qp2-2006 to 2013 (+৳ 490)
+
+ IG-ACC-qp2-2002 to 2005 (+৳ 90)
+
+ IG-ACC-qp3-2002 to 2009 (+৳ 270)
+
+Accounting (9-1) 0985
+ IG-ACC-qp1-2020 to 2024 (+৳ 190)
+
+ IG-ACC-qp2-2020 to 2024 (+৳ 270)
+
+Additional math 0606
+ IG-ADD-qp1-2020 to 2024 (+৳ 480)
+
+ IG-ADD-qp1-2015 to 2019 (+৳ 490)
+
+ IG-ADD-qp1-2002 to 2014 (+৳ 480)
+
+ IG-ADD-qp2-2020 to 2024 (+৳ 470)
+
+ IG-ADD-qp2-2015 to 2019 (+৳ 490)
+
+ IG-ADD-qp2-2002 to 2014 (+৳ 480)
+
+biology-0610
+ IG-BIO-qp1-2020 to 2024 (+৳ 490)
+
+ IG-BIO-qp1-2016 to 2019 (+৳ 440)
+
+ IG-BIO-qp1-2011 to 2015 (+৳ 490)
+
+ IG-BIO-qp1-2002 to 2010 (+৳ 380)
+
+ IG-BIO-qp2-2020 to 2024 (+৳ 480)
+
+ IG-BIO-qp2-2016 to 2019 (+৳ 440)
+
+ IG-BIO-qp2-2011 to 2015 (+৳ 530)
+
+ IG-BIO-qp2-2002 to 2010 (+৳ 290)
+
+ IG-BIO-qp3-2021 to 2024 (+৳ 440)
+
+ IG-BIO-qp3-2017 to 2020 (+৳ 470)
+
+ IG-BIO-qp3-2013 to 2016 (+৳ 470)
+
+ IG-BIO-qp3-2006 to 2012 (+৳ 490)
+
+ IG-BIO-qp3-2002 to 2005 (+৳ 90)
+
+ IG-BIO-qp4-2021 to 2024 (+৳ 440)
+
+ IG-BIO-qp4-2017 to 2020 (+৳ 480)
+
+ IG-BIO-qp4-2016 to 2016 (+৳ 160)
+
+ IG-BIO-qp5-2018 to 2024 (+৳ 470)
+
+ IG-BIO-qp5-2007 to 2017 (+৳ 490)
+
+ IG-BIO-qp5-2002 to 2006 (+৳ 90)
+
+ IG-BIO-qp6-2018 to 2024 (+৳ 490)
+
+ IG-BIO-qp6-2009 to 2017 (+৳ 530)
+
+ IG-BIO-qp6-2002 to 2008 (+৳ 160)
+
+biology (9-1)-0970
+ IG-BIO-qp1-2018 to 2023 (+৳ 240)
+
+ IG-BIO-qp2-2018 to 2023 (+৳ 270)
+
+ IG-BIO-qp3-2018 to 2023 (+৳ 260)
+
+ IG-BIO-qp4-2018 to 2023 (+৳ 290)
+
+ IG-BIO-qp5-2018 to 2023 (+৳ 130)
+
+ IG-BIO-qp6-2018 to 2023 (+৳ 190)
+
+Business Studies-0450
+ IG-BUS-qp1-2017 to 2024 (+৳ 530)
+
+ IG-BUS-qp1-2010 to 2016 (+৳ 440)
+
+ IG-BUS-qp2-2019 to 2024 (+৳ 490)
+
+ IG-BUS-qp2-2013 to 2018 (+৳ 490)
+
+ IG-BUS-qp2-2010 to 2012 (+৳ 260)
+
+Business Studie (9-1)-0986
+ IG-BUS-qp1-2019 to 2023 (+৳ 160)
+
+ IG-BUS-qp2-2019 to 2023 (+৳ 190)
+
+Chemistry-0620
+ IG-CHE-qp1-2020 to 2024 (+৳ 450)
+
+ IG-CHE-qp1-2015 to 2019 (+৳ 490)
+
+ IG-CHE-qp1-2009 to 2014 (+৳ 490)
+
+ IG-CHE-qp1-2002 to 2008 (+৳ 240)
+
+ IG-CHE-qp2-2020 to 2024 (+৳ 490)
+
+ IG-CHE-qp2-2015 to 2019 (+৳ 490)
+
+ IG-CHE-qp2-2007 to 2014 (+৳ 490)
+
+ IG-CHE-qp2-2002 to 2006 (+৳ 180)
+
+ IG-CHE-qp3-2021 to 2024 (+৳ 430)
+
+ IG-CHE-qp3-2017 to 2020 (+৳ 460)
+
+ IG-CHE-qp3-2011 to 2016 (+৳ 480)
+
+ IG-CHE-qp3-2002 to 2010 (+৳ 340)
+
+ IG-CHE-qp4-2020 to 2024 (+৳ 440)
+
+ IG-CHE-qp4-2016 to 2019 (+৳ 390)
+
+ IG-CHE-qp5-2018 to 2024 (+৳ 480)
+
+ IG-CHE-qp5-2002 to 2017 (+৳ 470)
+
+ IG-CHE-qp6-2018 to 2024 (+৳ 490)
+
+ IG-CHE-qp6-2011 to 2017 (+৳ 460)
+
+ IG-CHE-qp6-2002 to 2010 (+৳ 280)
+
+Chemistry (9-1)-0971
+ IG-CHE-qp1-2018 to 2023 (+৳ 190)
+
+ IG-CHE-qp2-2018 to 2023 (+৳ 270)
+
+ IG-CHE-qp3-2018 to 2023 (+৳ 250)
+
+ IG-CHE-qp4-2018 to 2023 (+৳ 240)
+
+ IG-CHE-qp5-2018 to 2023 (+৳ 140)
+
+ IG-CHE-qp6-2018 to 2023 (+৳ 190)
+
+Computer Science-0478
+ IG-COM-qp1-2018 to 2024 (+৳ 490)
+
+ IG-COM-qp1-2015 to 2017 (+৳ 260)
+
+ IG-COM-qp2-2019 to 2024 (+৳ 490)
+
+ IG-COM-qp2-2015 to 2018 (+৳ 290)
+
+Computer Science (9-1)-0984
+ IG-COM-qp1-2019 to 2023 (+৳ 170)
+
+ IG-COM-qp2-2019 to 2023 (+৳ 190)
+
+English – Second Language (9-1)-0991
+ IG-ENG-qp1-2019 to 2024 (+৳ 240)
+
+ IG-ENG-qp2-2019 to 2024 (+৳ 250)
+
+ IG-ENG-qp3-2019 to 2023 (+৳ 140)
+
+ IG-ENG-qp4-2019 to 2023 (+৳ 170)
+
+English Literature in English -0475
+ IG-ENG-qp1-2021 to 2024 (+৳ 490)
+
+ IG-ENG-qp1-2020 to 2020 (+৳ 190)
+
+ IG-ENG-qp2-2020 to 2024 (+৳ 340)
+
+ IG-ENG-qp3-2020 to 2024 (+৳ 340)
+
+ IG-ENG-qp4-2020 to 2024 (+৳ 190)
+
+English Literature in English (9-1) 0992
+ IG-ENG-qp1-2019 to 2024 (+৳ 290)
+
+ IG-ENG-qp2-2019 to 2024 (+৳ 160)
+
+ IG-ENG-qp3-2019 to 2024 (+৳ 160)
+
+ IG-ENG-qp4-2019 to 2024 (+৳ 90)
+
+economics 9-1 -0987
+ IG-ECO-qp1-2020 to 2024 (+৳ 180)
+
+ IG-ECO-qp2-2020 to 2024 (+৳ 90)
+
+economics-0455
+ IG-ECO-qp1-2018 to 2024 (+৳ 490)
+
+ IG-ECO-qp1-2008 to 2017 (+৳ 530)
+
+ IG-ECO-qp1-2002 to 2007 (+৳ 170)
+
+ IG-ECO-qp2-2002 to 2024 (+৳ 480)
+
+ IG-ECO-qp3-2002 to 2013 (+৳ 390)
+
+ IG-ECO-qp4-2002 to 2008 (+৳ 90)
+
+ IG-ECO-qp6-2002 to 2008 (+৳ 90)
+
+English 0500
+ IG-ENG-qp1-2022-2024 (+৳ 420)
+
+ IG-ENG-qp1-2018-2021 (+৳ 460)
+
+ IG-ENG-qp1-2014-2017 (+৳ 350)
+
+ IG-ENG-qp2-2020-2024 (+৳ 440)
+
+ IG-ENG-qp2-2016-2019 (+৳ 450)
+
+ IG-ENG-qp2-2014-2015 (+৳ 230)
+
+ IG-ENG-qp3-2015-2019 (+৳ 470)
+
+ IG-ENG-qp3-2014-2014 (+৳ 110)
+
+English as a Second Language (Speaking endorsement)-0510
+ IG-ENG-qp1-2018 to 2024 (+৳ 490)
+
+ IG-ENG-qp1-2014 to 2017 (+৳ 330)
+
+ IG-ENG-qp2-2018 to 2024 (+৳ 490)
+
+ IG-ENG-qp2-2014 to 2017 (+৳ 380)
+
+ IG-ENG-qp3-2014 to 2023 (+৳ 390)
+
+ IG-ENG-qp4-2014 to 2023 (+৳ 490)
+
+English as a Second Language (Count-in speaking)-0511
+ IG-ENG-qp1-2023 to 2024 (+৳ 130)
+
+ IG-ENG-qp2-2023 to 2024 (+৳ 90)
+
+ IG-ENG-qp3-2023 to 2023 (+৳ 70)
+
+ IG-ENG-qp4-2023 to 2023 (+৳ 70)
+
+English as a Second Language (Speaking endorsement (9-1)-0993
+ IG-ENG-qp1-2023 to 2024 (+৳ 90)
+
+ IG-ENG-qp2-2023 to 2024 (+৳ 90)
+
+ IG-ENG-qp3-2023 to 2023 (+৳ 60)
+
+ IG-ENG-qp4-2023 to 2023 (+৳ 60)
+
+Environmental Management 0680
+ IG-ENV-qp1-2021 to 2024 (+৳ 440)
+
+ IG-ENV-qp1-2016 to 2020 (+৳ 490)
+
+ IG-ENV-qp1-2008 to 2015 (+৳ 490)
+
+ IG-ENV-qp1-2004 to 2007 (+৳ 140)
+
+ IG-ENV-qp2-2021 to 2024 (+৳ 480)
+
+ IG-ENV-qp2-2017 to 2020 (+৳ 490)
+
+ IG-ENV-qp2-2012 to 2016 (+৳ 490)
+
+ IG-ENV-qp2-2004 to 2011 (+৳ 370)
+
+ IG-ENV-qp4-2014 to 2018 (+৳ 480)
+
+ IG-ENV-qp4-2004 to 2013 (+৳ 530)
+
+ICT-0417
+Information and Communication Technology ict
+
+ IG-INF-qp1-2020 to 2024 (+৳ 470)
+
+ IG-INF-qp1-2015 to 2019 (+৳ 490)
+
+ IG-INF-qp1-2010 to 2014 (+৳ 390)
+
+ IG-INF-qp2-2010 to 2024 (+৳ 490)
+
+ IG-INF-qp3-2010 to 2024 (+৳ 380)
+
+ICT 9-1-0983
+Information and Communication Technology ict
+
+ IG-INF-qp1-2020 to 2024 (+৳ 170)
+
+ IG-INF-qp2-2020 to 2024 (+৳ 190)
+
+ IG-INF-qp3-2020 to 2024 (+৳ 140)
+
+MATH US-0444
+ IG-MAT-qp1-2016 to 2020 (+৳ 130)
+
+ IG-MAT-qp2-2016 to 2023 (+৳ 190)
+
+ IG-MAT-qp3-2016 to 2020 (+৳ 170)
+
+ IG-MAT-qp4-2016 to 2023 (+৳ 290)
+
+MATH-0580
+ IG-MAT-qp1-2018 to 2024 (+৳ 480)
+
+ IG-MAT-qp1-2011 to 2017 (+৳ 480)
+
+ IG-MAT-qp1-2002 to 2010 (+৳ 290)
+
+ IG-MAT-qp2-2019 to 2024 (+৳ 460)
+
+ IG-MAT-qp2-2013 to 2018 (+৳ 480)
+
+ IG-MAT-qp2-2002 to 2012 (+৳ 430)
+
+ IG-MAT-qp3-2021 to 2024 (+৳ 440)
+
+ IG-MAT-qp3-2017 to 2020 (+৳ 450)
+
+ IG-MAT-qp3-2012 to 2016 (+৳ 490)
+
+ IG-MAT-qp3-2002 to 2011 (+৳ 380)
+
+ IG-MAT-qp4-2021 to 2024 (+৳ 470)
+
+ IG-MAT-qp4-2017 to 2020 (+৳ 490)
+
+ IG-MAT-qp4-2013 to 2016 (+৳ 460)
+
+ IG-MAT-qp4-2002 to 2012 (+৳ 470)
+
+MATH INTERNATIONAL-0607
+ IG-MAT-qp1-2015 to 2024 (+৳ 490)
+
+ IG-MAT-qp1-2009 to 2014 (+৳ 190)
+
+ IG-MAT-qp2-2015 to 2024 (+৳ 490)
+
+ IG-MAT-qp2-2009 to 2014 (+৳ 190)
+
+ IG-MAT-qp3-2020 to 2024 (+৳ 470)
+
+ IG-MAT-qp3-2015 to 2019 (+৳ 480)
+
+ IG-MAT-qp3-2009 to 2014 (+৳ 340)
+
+ IG-MAT-qp4-2021 to 2024 (+৳ 470)
+
+ IG-MAT-qp4-2017 to 2020 (+৳ 450)
+
+ IG-MAT-qp4-2012 to 2016 (+৳ 490)
+
+ IG-MAT-qp4-2009 to 2011 (+৳ 150)
+
+ IG-MAT-qp5-2010 to 2024 (+৳ 530)
+
+ IG-MAT-qp5-2009 to 2009 (+৳ 50)
+
+ IG-MAT-qp6-2019 to 2024 (+৳ 490)
+
+ IG-MAT-qp6-2009 to 2018 (+৳ 440)
+
+MATH (9-1)-0980
+ IG-MAT-qp1-2019 to 2023 (+৳ 140)
+
+ IG-MAT-qp2-2019 to 2023 (+৳ 150)
+
+ IG-MAT-qp3-2019 to 2023 (+৳ 190)
+
+ IG-MAT-qp4-2019 to 2023 (+৳ 190)
+
+physics-0625
+ IG-PHY-qp1-2020 to 2024 (+৳ 490)
+
+ IG-PHY-qp1-2016 to 2019 (+৳ 470)
+
+ IG-PHY-qp1-2012 to 2015 (+৳ 450)
+
+ IG-PHY-qp1-2002 to 2011 (+৳ 490)
+
+ IG-PHY-qp2-2020 to 2024 (+৳ 490)
+
+ IG-PHY-qp2-2016 to 2019 (+৳ 450)
+
+ IG-PHY-qp2-2011 to 2015 (+৳ 490)
+
+ IG-PHY-qp2-2002 to 2010 (+৳ 360)
+
+ IG-PHY-qp3-2020 to 2024 (+৳ 480)
+
+ IG-PHY-qp3-2016 to 2019 (+৳ 470)
+
+ IG-PHY-qp3-2011 to 2015 (+৳ 490)
+
+ IG-PHY-qp3-2002 to 2010 (+৳ 390)
+
+ IG-PHY-qp4-2020 to 2024 (+৳ 480)
+
+ IG-PHY-qp4-2016 to 2019 (+৳ 460)
+
+ IG-PHY-qp5-2018 to 2024 (+৳ 490)
+
+ IG-PHY-qp5-2009 to 2017 (+৳ 490)
+
+ IG-PHY-qp5-2002 to 2008 (+৳ 150)
+
+ IG-PHY-qp6-2018 to 2024 (+৳ 530)
+
+ IG-PHY-qp6-2012 to 2017 (+৳ 490)
+
+ IG-PHY-qp6-2002 to 2011 (+৳ 290)
+
+physics (9-1)-0972
+ IG-PHY-qp1-2018 to 2023 (+৳ 240)
+
+ IG-PHY-qp2-2018 to 2023 (+৳ 260)
+
+ IG-PHY-qp3-2018 to 2023 (+৳ 240)
+
+ IG-PHY-qp4-2018 to 2023 (+৳ 260)
+
+ IG-PHY-qp5-2018 to 2023 (+৳ 140)
+
+ IG-PHY-qp6-2018 to 2023 (+৳ 190)
+
+sociology-0495
+ IG-SOC-qp1-2016 to 2024 (+৳ 190)
+
+ IG-SOC-qp2-2016 to 2024 (+৳ 190)
+
+islamiyat-0493
+ IG-ISL-qp1-2016 to 2024 (+৳ 490)
+
+ IG-ISL-qp2-2016 to 2024 (+৳ 490)
+================================================================================
+Accounting 7707
+ OL-ACC-qp1-2012 to 2024 (+৳ 490)
+
+ OL-ACC-qp1-2002 to 2011 (+৳ 250)
+
+ OL-ACC-qp2-2018 to 2024 (+৳ 490)
+
+ OL-ACC-qp2-2011 to 2017 (+৳ 530)
+
+ OL-ACC-qp2-2002 to 2010 (+৳ 280)
+
+Additional Maths 4037
+ OL-ADD-qp1-2016 to 2024 (+৳ 490)
+
+ OL-ADD-qp1-2002 to 2015 (+৳ 460)
+
+ OL-ADD-qp2-2016 to 2024 (+৳ 490)
+
+ OL-ADD-qp2-2002 to 2015 (+৳ 450)
+
+BENGALI 3204
+ OL-BEN-qp1 and qp2-2004 to 2024 (+৳ 450)
+
+BD Studies 7094
+ OL-BDS-qp1-2006 to 2024 (+৳ 160)
+
+ OL-BDS-qp2-2006 to 2024 (+৳ 330)
+
+ OL-BDS-P2-2010 to 2024 (Color Inserts) (+৳ 220)
+
+BIOLOGY 5090
+ OL-BIO-qp1-2017 to 2024 (+৳ 490)
+
+ OL-BIO-qp1-2010 to 2016 (+৳ 490)
+
+ OL-BIO-qp1-2002 to 2009 (+৳ 280)
+
+ OL-BIO-qp2-2016 to 2024 (+৳ 480)
+
+ OL-BIO-qp2-2002 to 2015 (+৳ 490)
+
+ OL-BIO-qp3-2008 to 2024 (practical) (+৳ 490)
+
+ OL-BIO-qp3-2002 to 2007 (practical) (+৳ 90)
+
+ OL-BIO-qp4-2010 to 2024 (Alternative to Practical) (+৳ 490)
+
+ OL-BIO-qp4-2002 to 2009 (Alternative to Practical) (+৳ 160)
+
+BUSINESS 7115
+ OL-BUS-qp1-2012 to 2024 (+৳ 490)
+
+ OL-BUS-qp1-2010 to 2011 (+৳ 130)
+
+ OL-BUS-qp2-2015 to 2024 (+৳ 490)
+
+ OL-BUS-qp2-2010 to 2014 (+৳ 290)
+
+CHEMISTRY 5070
+ OL-CHE-qp1-2016 to 2024 (+৳ 490)
+
+ OL-CHE-qp1-2005 to 2015 (+৳ 490)
+
+ OL-CHE-qp1-2002 to 2004 (+৳ 90)
+
+ OL-CHE-qp2-2017 to 2024 (+৳ 490)
+
+ OL-CHE-qp2-2010 to 2016 (+৳ 490)
+
+ OL-CHE-qp2-2002 to 2009 (+৳ 270)
+
+ OL-CHE-qp3-2002 to 2024 (+৳ 490)
+
+ OL-CHE-qp4-2015 to 2024 (+৳ 490)
+
+ OL-CHE-qp4-2004 to 2014 (+৳ 490)
+
+ OL-CHE-qp4-2002 to 2003 (+৳ 90)
+
+COMMERCE 7100
+ OL-CMR-qp1-2013 to 2024 (+৳ 490)
+
+ OL-CMR-qp1-2002 to 2012 (+৳ 290)
+
+ OL-CMR-qp2-2010 to 2024 (+৳ 490)
+
+ OL-CMR-qp2-2005 to 2009 (+৳ 130)
+
+COMPUTER SCIENCE 2210 (CURRENT)
+ OL-COM-qp1-2015 to 2024 (+৳ 430)
+
+ OL-COM-qp2-2015 to 2024 (+৳ 450)
+
+MATHS D 4024
+ OL-DMA-qp1-2017 to 2024 (+৳ 490)
+
+ OL-DMA-qp1-2011 to 2016 (+৳ 470)
+
+ OL-DMA-qp1-2002 to 2010 (+৳ 370)
+
+ OL-DMA-qp2-2018 to 2024 (+৳ 490)
+
+ OL-DMA-qp2-2012 to 2017 (+৳ 490)
+
+ OL-DMA-qp2-2002 to 2011 (+৳ 330)
+
+ECONOMICS 2281
+ OL-ECO-qp1-2013 to 2024 (+৳ 490)
+
+ OL-ECO-qp1-2003 to 2012 (+৳ 340)
+
+ OL-ECO-qp2-2002 to 2024 (+৳ 350)
+
+ENGLISH 1123
+ OL-ENG-qp1-2002 to 2024 (+৳ 260)
+
+ OL-ENG-qp2-2012 to 2024 (+৳ 490)
+
+ OL-ENG-qp2-2002 to 2011 (+৳ 190)
+
+ENVIRONMENT 5014
+ OL-ENV-qp1-2016 to 2024 (+৳ 490)
+
+ OL-ENV-qp1-2011 to 2015 (+৳ 450)
+
+ OL-ENV-qp1-2004 to 2010 (+৳ 380)
+
+ OL-ENV-qp2-2015 to 2024 (+৳ 490)
+
+ OL-ENV-qp2-2004 to 2014 (+৳ 480)
+
+ISLAMIYAT 2058
+ OL-ISL-qp1-2017 to 2024 (+৳ 90)
+
+ OL-ISL-qp1-2016 to 2024 (+৳ 490)
+
+ OL-ISL-qp1-2002 to 2015 (+৳ 290)
+
+ OL-ISL-qp2-2017 to 2024 (+৳ 90)
+
+ OL-ISL-qp2-2016 to 2024 (+৳ 490)
+
+ OL-ISL-qp2-2003 to 2015 (+৳ 290)
+
+ OL-ISL-qp4-2013 to 2013 (+৳ 80)
+
+ OL-LIT-qp1-2023 to 2024 (+৳ 140)
+
+ OL-LIT-qp2-2023 to 2024 (+৳ 90)
+
+PHYSICS 5054
+ OL-PHY-qp1-2017 to 2024 (+৳ 480)
+
+ OL-PHY-qp1-2008 to 2016 (+৳ 490)
+
+ OL-PHY-qp1-2002 to 2007 (+৳ 190)
+
+ OL-PHY-qp2-2017 to 2024 (+৳ 490)
+
+ OL-PHY-qp2-2008 to 2016 (+৳ 490)
+
+ OL-PHY-qp2-2002 to 2007 (+৳ 190)
+
+ OL-PHY-qp3-2013 to 2024 (+৳ 490)
+
+ OL-PHY-qp3-2002 to 2012 (+৳ 250)
+
+ OL-PHY-qp4-2011 to 2024 (+৳ 490)
+
+ OL-PHY-qp4-2002 to 2010 (+৳ 190)
+
+Sociology 2251
+ OL-SOC-qp1-2002 to 2024 (+৳ 490)
+
+ OL-SOC-qp2-2011 to 2024 (+৳ 470)
+
+ OL-SOC-qp2-2002 to 2010 (+৳ 170)
+
+Statistics 4040
+ OL-STA-qp1-2009 to 2023 (+৳ 530)
+
+ OL-STA-qp1-2002 to 2008 (+৳ 90)
+
+ OL-STA-qp2-2008 to 2023 (+৳ 490)
+
+ OL-STA-qp2-2002 to 2007 (+৳ 90)
+================================================================================
+
+
+ACCOUNTING 9706
+“AS (Paper 1 and Paper 2) || A2 (Paper 3 and paper 4)
 A2 Paper 4 (problem solving discontinued from 2016)
-(note: Paper 3 2002 to 2015 is MCQ,not needed for current candidates due to syllabus change)"
+(note: Paper 3 2002 to 2015 is MCQ,not needed for current candidates due to syllabus change)”
 
-AL-ACC-qp1-2018 to 2024 (+৳ 470)
+ AL-ACC-qp1-2018 to 2024 (+৳ 470)
 
-AL-ACC-qp1-2009 to 2017 (+৳ 530)
+ AL-ACC-qp1-2009 to 2017 (+৳ 530)
 
-AL-ACC-qp1-2002 to 2008 (+৳ 180)
+ AL-ACC-qp1-2002 to 2008 (+৳ 180)
 
-AL-ACC-qp2-2021 to 2024 (+৳ 430)
+ AL-ACC-qp2-2021 to 2024 (+৳ 430)
 
-AL-ACC-qp2-2017 to 2020 (+৳ 480)`;
+ AL-ACC-qp2-2017 to 2020 (+৳ 480)
+
+ AL-ACC-qp2-2011 to 2016 (+৳ 490)
+
+ AL-ACC-qp2-2001 to 2010 (+৳ 290)
+
+ AL-ACC-qp3-2022 to 2024 (+৳ 390)
+
+ AL-ACC-qp3-2020 to 2021 (+৳ 490)
+
+ AL-ACC-qp3-2013 to 2019 (+৳ 490)
+
+ AL-ACC-qp3-2002 to 2012 (+৳ 390)
+
+ AL-ACC-qp4-2002 to 2024 (+৳ 460)
+
+BUSINESS 9609
+AS Paper 1,2 || A2 Paper 3
+
+ AL-BUS-qp1-2016 to 2024 (+৳ 160)
+
+ AL-BUS-qp2-2016 to 2024 (+৳ 230)
+
+ AL-BUS-qp3-2021 to 2024 (+৳ 460)
+
+ AL-BUS-qp3-2018 to 2020 (+৳ 490)
+
+ AL-BUS-qp3-2016 to 2017 (+৳ 130)
+
+ AL-BUS-qp4-2023 to 2024 (+৳ 80)
+
+BIOLOGY 9700
+AS Paper 1,2,3 || A2 Paper 4,5
+
+ AL-BIO-qp1-2021 to 2024 (+৳ 440)
+
+ AL-BIO-qp1-2017 to 2020 (+৳ 460)
+
+ AL-BIO-qp1-2012 to 2016 (+৳ 490)
+
+ AL-BIO-qp1-2002 to 2011 (+৳ 450)
+
+ AL-BIO-qp2-2020 to 2024 (+৳ 490)
+
+ AL-BIO-qp2-2015 to 2019 (+৳ 480)
+
+ AL-BIO-qp2-2007 to 2014 (+৳ 490)
+
+ AL-BIO-qp2-2001 to 2006 (+৳ 170)
+
+ AL-BIO-qp3-2021 to 2024 (+৳ 490)
+
+ AL-BIO-qp3-2018 to 2020 (+৳ 390)
+
+ AL-BIO-qp3-2014 to 2017 (+৳ 490)
+
+ AL-BIO-qp3-2009 to 2013 (+৳ 490)
+
+ AL-BIO-qp3-2001 to 2008 (+৳ 180)
+
+ AL-BIO-qp4-2021 to 2024 (+৳ 490)
+
+ AL-BIO-qp4-2018 to 2020 (+৳ 440)
+
+ AL-BIO-qp4-2013 to 2017 (+৳ 490)
+
+ AL-BIO-qp4-2003 to 2012 (+৳ 490)
+
+ AL-BIO-qp4-2002 to 2002 (+৳ 70)
+
+ AL-BIO-qp5-2017 to 2024 (+৳ 490)
+
+ AL-BIO-qp5-2002 to 2016 (+৳ 360)
+
+ AL-BIO-qp6-2002 to 2006 (+৳ 290)
+
+CHEMISTRY 9701
+AS Paper 1 , 2 , 3 || A2 Paper 4 , 5
+
+ AL-CHE-qp1-2002 to 2005 (+৳ 150)
+
+ AL-CHE-qp1-2006 to 2014 (+৳ 530)
+
+ AL-CHE-qp1-2015 to 2019 (+৳ 480)
+
+ AL-CHE-qp1-2020 to 2024 (+৳ 490)
+
+ AL-CHE-qp2-2002 to 2012 (+৳ 380)
+
+ AL-CHE-qp2-2013 to 2019 (+৳ 490)
+
+ AL-CHE-qp2-2020 to 2024 (+৳ 470)
+
+ AL-CHE-qp3-2001 to 2008 (+৳ 190)
+
+ AL-CHE-qp3-2009 to 2013 (+৳ 490)
+
+ AL-CHE-qp3-2014 to 2017 (+৳ 490)
+
+ AL-CHE-qp3-2018 to 2020 (+৳ 440)
+
+ AL-CHE-qp3-2021 to 2024 (+৳ 470)
+
+ AL-CHE-qp4-2002 to 2006 (+৳ 150)
+
+ AL-CHE-qp4-2007 to 2015 (+৳ 530)
+
+ AL-CHE-qp4-2016 to 2020 (+৳ 490)
+
+ AL-CHE-qp4-2021 to 2024 (+৳ 460)
+
+ AL-CHE-qp5-2002 to 2014 (+৳ 370)
+
+ AL-CHE-qp5-2015 to 2024 (+৳ 530)
+
+ AL-CHE-qp6-2002 to 2006 (+৳ 130)
+
+COMPUTER 9618 (Current)
+AS Paper 1,2 || A2 Paper 3,4 (latest)
+
+ AL-COM-qp1-2021 to 2024 (+৳ 290)
+
+ AL-COM-qp2-2021 to 2024 (+৳ 440)
+
+ AL-COM-qp3-2021 to 2024 (+৳ 270)
+
+ AL-COM-qp4-2021 to 2024 (+৳ 240)
+
+COMPUTER 9608
+AS Paper 1,2 || A2 Paper 3,4
+
+ AL-COM-qp1-2015 to 2015 (+৳ 130)
+
+ AL-COM-qp1-2016 to 2021 (+৳ 490)
+
+ AL-COM-qp2-2015 to 2016 (+৳ 240)
+
+ AL-COM-qp2-2017 to 2021 (+৳ 490)
+
+ AL-COM-qp3-2015 to 2021 (+৳ 530)
+
+ AL-COM-qp4-2015 to 2016 (+৳ 230)
+
+ AL-COM-qp4-2017 to 2021 (+৳ 490)
+
+ECONOMICS 9708
+AS Paper 1,2 || A2 Paper 3,4
+
+ AL-ECO-qp1-2002 to 2010 (+৳ 290)
+
+ AL-ECO-qp1-2011 to 2017 (+৳ 490)
+
+ AL-ECO-qp1-2018 to 2024 (+৳ 490)
+
+ AL-ECO-qp2-2001 to 2024 (+৳ 390)
+
+ AL-ECO-qp3-2002 to 2010 (+৳ 290)
+
+ AL-ECO-qp3-2011 to 2017 (+৳ 490)
+
+ AL-ECO-qp3-2018 to 2024 (+৳ 490)
+
+ AL-ECO-qp4-2002 to 2024 (+৳ 390)
+
+Environment Management 8291
+ AL-ENV-qp2-2018 to 2024 (+৳ 490)
+
+ AL-ENV-qp2-2013 to 2017 (+৳ 290)
+
+ AL-ENV-qp1-2018 to 2024 (+৳ 490)
+
+ AL-ENV-qp1-2013 to 2017 (+৳ 280)
+
+FURTHER MATH 9231
+ AL-FPM-qp4-2020 to 2024 (+৳ 350)
+
+ AL-FPM-qp3-2020 to 2024 (+৳ 380)
+
+ AL-FPM-qp2-2020 to 2024 (+৳ 390)
+
+ AL-FPM-qp2-2013 to 2019 (+৳ 490)
+
+ AL-FPM-qp2-2002 to 2012 (+৳ 190)
+
+ AL-FPM-qp1-2020 to 2024 (+৳ 390)
+
+ AL-FPM-qp1-2013 to 2019 (+৳ 530)
+
+ AL-FPM-qp1-2002 to 2012 (+৳ 190)
+
+INFORMATION TECHNOLOGY 9626
+INFORMATION TECHNOLOGY PASTPAPER 9626 (exam started from 2017)
+
+ AL-ITE-qp1-2019 to 2024 (+৳ 480)
+
+ AL-ITE-qp1-2017 to 2018 (+৳ 230)
+
+ AL-ITE-qp2-2017 to 2024 (+৳ 150)
+
+ AL-ITE-qp3-2018 to 2024 (+৳ 490)
+
+ AL-ITE-qp3-2017 to 2017 (+৳ 100)
+
+ AL-ITE-qp4-2017 to 2024 (+৳ 190)
+
+LAW 9084
+AS Paper 1,2 || A2 Paper 3,4
+
+ AL-LAW-qp4-2004 to 2024 (+৳ 290)
+
+ AL-LAW-qp3-2004 to 2024 (+৳ 290)
+
+ AL-LAW-qp2-2002 to 2024 (+৳ 390)
+
+ AL-LAW-qp1-2002 to 2024 (+৳ 190)
+
+MATH 9709
+ AL-MAT-qp1-2002 to 2016 (+৳ 260)
+
+ AL-MAT-qp1-2017 to 2020 (+৳ 490)
+
+ AL-MAT-qp1-2021 to 2024 (+৳ 450)
+
+ AL-MAT-qp2-2001 to 2018 (+৳ 370)
+
+ AL-MAT-qp2-2019 to 2024 (+৳ 480)
+
+ AL-MAT-qp3-2002 to 2015 (+৳ 190)
+
+ AL-MAT-qp3-2016 to 2020 (+৳ 530)
+
+ AL-MAT-qp3-2021 to 2024 (+৳ 450)
+
+ AL-MAT-qp4-2001 to 2018 (Mechanics) (+৳ 390)
+
+ AL-MAT-qp4-2019 to 2024 (Mechanics) (+৳ 480)
+
+ AL-MAT-qp5-2001 to 2018 (Statistics 1) (+৳ 370)
+
+ AL-MAT-qp5-2019 to 2024 (Statistics 1) (+৳ 480)
+
+ AL-MAT-qp6-2002 to 2018 (Statistics 2) (+৳ 360)
+
+ AL-MAT-qp6-2019 to 2024 (Statistics 2) (+৳ 460)
+
+Marine Science 9693
+ AL-MAR-qp4-2009 to 2024 (+৳ 490)
+
+ AL-MAR-qp3-2012 to 2024 (+৳ 490)
+
+ AL-MAR-qp3-2009 to 2011 (+৳ 90)
+
+ AL-MAR-qp2-2014 to 2024 (+৳ 490)
+
+ AL-MAR-qp2-2008 to 2013 (+৳ 140)
+
+ AL-MAR-qp1-2015 to 2024 (+৳ 490)
+
+ AL-MAR-qp1-2008 to 2014 (+৳ 190)
+
+PSYCHOLOGY 9990 (Current)
+Starts from 2018 (first exam)
+
+ AL-PSY-qp1-2018 to 2024 (+৳ 390)
+
+ AL-PSY-qp2-2018 to 2024 (+৳ 450)
+
+ AL-PSY-qp3-2018 to 2024 (+৳ 170)
+
+ AL-PSY-qp4-2018 to 2024 (+৳ 250)
+
+PHYCHOLOGY 9698
+Ends in 2018 (Last exam)
+
+ AL-PSY-qp1-2002 to 2018 (+৳ 290)
+
+ AL-PSY-qp2-2002 to 2018 (+৳ 250)
+
+ AL-PSY-qp3-2002 to 2002 (+৳ 60)
+
+ AL-PSY-qp3-2003 to 2018 (+৳ 490)
+
+ AL-PSY-qp4-2018 to 2018 (+৳ 80)
+
+PHYSICS 9702
+ AL-PHY-qp1-2002 to 2007 (+৳ 240)
+
+ AL-PHY-qp1-2008 to 2012 (+৳ 490)
+
+ AL-PHY-qp1-2013 to 2016 (+৳ 530)
+
+ AL-PHY-qp1-2017 to 2020 (+৳ 530)
+
+ AL-PHY-qp1-2021 to 2024 (+৳ 460)
+
+ AL-PHY-qp2-2002 to 2007 (+৳ 190)
+
+ AL-PHY-qp2-2008 to 2014 (+৳ 490)
+
+ AL-PHY-qp2-2015 to 2019 (+৳ 490)
+
+ AL-PHY-qp2-2020 to 2024 (+৳ 490)
+
+ AL-PHY-qp3-2002 to 2011 (+৳ 380)
+
+ AL-PHY-qp3-2012 to 2016 (+৳ 530)
+
+ AL-PHY-qp3-2017 to 2020 (+৳ 470)
+
+ AL-PHY-qp3-2021 to 2024 (+৳ 460)
+
+ AL-PHY-qp4-2002 to 2011 (+৳ 470)
+
+ AL-PHY-qp4-2012 to 2016 (+৳ 480)
+
+ AL-PHY-qp4-2017 to 2020 (+৳ 480)
+
+ AL-PHY-qp4-2021 to 2024 (+৳ 470)
+
+ AL-PHY-qp5-2002 to 2009 (+৳ 180)
+
+ AL-PHY-qp5-2010 to 2024 (+৳ 490)
+
+ AL-PHY-qp6-2002 to 2006 (+৳ 190)
+
+Sociology 9699
+ AL-SOC-qp1-2002 to 2024 (+৳ 260)
+
+ AL-SOC-qp2-2002 to 2024 (+৳ 480)
+
+ AL-SOC-qp3-2002 to 2024 (+৳ 340)
+
+ AL-SOC-qp4-2021 to 2024 (+৳ 90)
+
+Thinking Skills 9694
+ AL-THI-qp1-2003 to 2008 (+৳ 290)
+
+ AL-THI-qp1-2009 to 2013 (+৳ 490)
+
+ AL-THI-qp1-2014 to 2018 (+৳ 490)
+
+ AL-THI-qp1-2019 to 2024 (+৳ 490)
+
+ AL-THI-qp2-2002 to 2005 (+৳ 80)
+
+ AL-THI-qp2-2006 to 2024 (+৳ 530)
+
+ AL-THI-qp3-2007 to 2012 (+৳ 380)
+
+ AL-THI-qp3-2013 to 2024 (+৳ 490)
+
+ AL-THI-qp4-2007 to 2011 (+৳ 190)
+
+ AL-THI-qp4-2012 to 2024 (+৳ 490)`;
